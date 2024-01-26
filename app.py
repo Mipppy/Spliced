bfea4f4bf4bb3f956051b8dcfeecdc51b1936ff7
Mipppy <122882588+Mipppy@users.noreply.github.com>
2024-01-26 10:44:36 -0500

    Enhaced error checking and error page

--- app.py
+++ app.py
@@ -18,56 +18,59 @@ def home():
     
 @app.route("/createaudio", methods=["POST"])
 def createfile():
-    textdata = request.form.get("text")
-    textdata = ' '.join(filter(None, textdata.replace('\r\n', ' ').split('\n')))
-    textdata = [word for word in textdata.split(" ") if word] 
-    character = request.form.get("character")
-    characterVoiceFiles = db.execute("SELECT voicelines.file, voicelines.name FROM character JOIN voicelines ON voicelines.character = character.name WHERE character.name = ?", character)
-    neededFiles,arrayOfFiles,notFoundWords,fullWords,neededSubAudioFiles,finalList = [],[],[],[],[],[]
-    for key in characterVoiceFiles:
-        arrayOfFiles.append(key)
-    arrayIter = list(arrayOfFiles)
-    arrayIter2 = list(textdata)
-    for string1 in arrayIter2:
-        found = False
-        for array in arrayIter:
+    try:
+        textdata = request.form.get("text")
+        textdata = ' '.join(filter(None, textdata.replace('\r\n', ' ').split('\n')))
+        textdata = [word for word in textdata.split(" ") if word] 
+        character = request.form.get("character")
+        characterVoiceFiles = db.execute("SELECT voicelines.file, voicelines.name FROM character JOIN voicelines ON voicelines.character = character.name WHERE character.name = ?", character)
+        neededFiles,arrayOfFiles,notFoundWords,fullWords,neededSubAudioFiles,finalList = [],[],[],[],[],[]
+        for key in characterVoiceFiles:
+            arrayOfFiles.append(key)
+        arrayIter = list(arrayOfFiles)
+        arrayIter2 = list(textdata)
+        for string1 in arrayIter2:
+            found = False
+            for array in arrayIter:
+                if found == True:
+                    continue
+                if str(array['name']).lower() == str(string1).lower():
+                    neededFiles.append(array['file'])
+                    fullWords.append(array['file'])
+                    found = True
+            if found == False:    
+                notFoundWords.append(str(string1).lower())
+                fullWords.append(str(string1).lower())
+        for word in notFoundWords:
+            subList = [word]
+            for char in word:
+                if char == None or char == "":
+                    continue
+                subList.append(subList.append(char + ".wav"))
+            
+            neededSubAudioFiles.append(list(filter(None, subList)))        
+        for word in fullWords:
+            found = False
+            finalArr,otherArray,finalSound = [],[],""
+            for arr in neededSubAudioFiles:
+                if str(arr[0]) == word:
+                    finalArr = arr
+                    found = True
             if found == True:
-                continue
-            if str(array['name']).lower() == str(string1).lower():
-                neededFiles.append(array['file'])
-                fullWords.append(array['file'])
-                found = True
-        if found == False:    
-            notFoundWords.append(str(string1).lower())
-            fullWords.append(str(string1).lower())
-    for word in notFoundWords:
-        subList = [word]
-        for char in word:
-            if char == None or char == "":
-                continue
-            subList.append(subList.append(char + ".wav"))
-        
-        neededSubAudioFiles.append(list(filter(None, subList)))        
-    for word in fullWords:
-        found = False
-        finalArr,otherArray,finalSound = [],[],""
-        for arr in neededSubAudioFiles:
-            if str(arr[0]) == word:
-                finalArr = arr
-                found = True
-        if found == True:
-            finalArr.pop(0)
-            for arr in finalArr:
-                finalList.append(arr)
-            finalList.append("")                
-        else:
-            finalList.append(word)
-            finalList.append("")
+                finalArr.pop(0)
+                for arr in finalArr:
+                    finalList.append(arr)
+                finalList.append("")                
+            else:
+                finalList.append(word)
+                finalList.append("")
 
-    randIntFor = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
-    concatenate_audio_wave(finalList, f"concatFiles\\{randIntFor}.wav",character)
-    @after_this_request
-    def deleteFileAfterReq(response):
-        threading.Thread(target=safe_del, args=(f"{dir_path}\\concatFiles\\{randIntFor}.wav",0)).start()
-        return response
-    return send_file(f"concatFiles\\{randIntFor}.wav", as_attachment=True, download_name=f"{character} Spliced.wav")
+        randIntFor = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
+        concatenate_audio_wave(finalList, f"concatFiles\\{randIntFor}.wav",character)
+        @after_this_request
+        def deleteFileAfterReq(response):
+            threading.Thread(target=safe_del, args=(f"{dir_path}\\concatFiles\\{randIntFor}.wav",0)).start()
+            return response
+        return send_file(f"concatFiles\\{randIntFor}.wav", as_attachment=True, download_name=f"{character} Spliced.wav")
+    except Exception as e:
+        return render_template("error.html")
--- helper.py
+++ helper.py
@@ -3,19 +3,21 @@ import wave,os,time
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def concatenate_audio_wave(audio_clip_paths, output_path,character):
-    data = []
-    for clip in audio_clip_paths:
-        if clip == "":
-            clip = ".wav"
-        w = wave.open(f"{dir_path}\\voiceclips\\{character}\\"+clip, "rb")
-        data.append([w.getparams(), w.readframes(w.getnframes())])
-        w.close()
-    output = wave.open(output_path, "wb")
-    output.setparams(data[0][0])
-    for i in range(len(data)):
-        output.writeframes(data[i][1])
-    output.close()
-
+    try:
+        data = []
+        for clip in audio_clip_paths:
+            if clip == "":
+                clip = ".wav"
+            w = wave.open(f"{dir_path}\\voiceclips\\{character}\\"+clip, "rb")
+            data.append([w.getparams(), w.readframes(w.getnframes())])
+            w.close()
+        output = wave.open(output_path, "wb")
+        output.setparams(data[0][0])
+        for i in range(len(data)):
+            output.writeframes(data[i][1])
+        output.close()
+    except Exception as e:
+        None
 def safe_del(filepath,recursionCounter):
     try:
         if os.path.exists(filepath):
