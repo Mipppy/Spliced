07ed161a038a4027403ff1623f617ecc52a0d68b
Mipppy <122882588+Mipppy@users.noreply.github.com>
2024-01-26 10:20:17 -0500

    Auto File deletion now works, and cleaned up some stuff

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, redirect, request,session, flash, send_file
-from flask_session import Session
+from flask import Flask, render_template, redirect, request, flash, send_file, after_this_request
 from cs50 import SQL
-import os,wave,random,string
+import os,wave,random,string, threading,time
+from helper import *
 dir_path = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 db = SQL("sqlite:///new.db")
 
 @app.route("/", methods=["GET", "POST"])
@@ -64,20 +63,11 @@ def createfile():
         else:
             finalList.append(word)
             finalList.append("")
+
     randIntFor = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
     concatenate_audio_wave(finalList, f"concatFiles\\{randIntFor}.wav",character)
-    return send_file(f"concatFiles\\{randIntFor}.wav", as_attachment=True)
-
-def concatenate_audio_wave(audio_clip_paths, output_path,character):
-    data = []
-    for clip in audio_clip_paths:
-        if clip == "":
-            clip = ".wav"
-        w = wave.open(f"{dir_path}\\voiceclips\\{character}\\"+clip, "rb")
-        data.append([w.getparams(), w.readframes(w.getnframes())])
-        w.close()
-    output = wave.open(output_path, "wb")
-    output.setparams(data[0][0])
-    for i in range(len(data)):
-        output.writeframes(data[i][1])
-    output.close()
+    @after_this_request
+    def deleteFileAfterReq(response):
+        threading.Thread(target=safe_del, args=(f"{dir_path}\\concatFiles\\{randIntFor}.wav",0)).start()
+        return response
+    return send_file(f"concatFiles\\{randIntFor}.wav", as_attachment=True, download_name=f"{character} Spliced.wav")
---
+++ helper.py
@@ -0,0 +1,30 @@
+import wave,os,time
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def concatenate_audio_wave(audio_clip_paths, output_path,character):
+    data = []
+    for clip in audio_clip_paths:
+        if clip == "":
+            clip = ".wav"
+        w = wave.open(f"{dir_path}\\voiceclips\\{character}\\"+clip, "rb")
+        data.append([w.getparams(), w.readframes(w.getnframes())])
+        w.close()
+    output = wave.open(output_path, "wb")
+    output.setparams(data[0][0])
+    for i in range(len(data)):
+        output.writeframes(data[i][1])
+    output.close()
+
+def safe_del(filepath,recursionCounter):
+    try:
+        if os.path.exists(filepath):
+            os.remove(filepath)
+            return True
+        else:
+            return False
+    except Exception as e:
+        time.sleep(1)
+        if recursionCounter < 15:
+            safe_del(filepath,recursionCounter+1)
+        return e
